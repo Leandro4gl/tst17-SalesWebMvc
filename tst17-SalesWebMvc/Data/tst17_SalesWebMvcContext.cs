c30ee6d9a8cc2de0467d0b9f6f1299e4f5f8f936
Leandro de Oliveira <leandro4gl@gmail.com>
2020-04-24 10:03:43 -0300

    CRUD Scaffolding

---
+++ tst17-SalesWebMvc/Data/tst17_SalesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using tst17_SalesWebMvc.Models;
+
+namespace tst17_SalesWebMvc.Data
+{
+    public class tst17_SalesWebMvcContext : DbContext
+    {
+        public tst17_SalesWebMvcContext (DbContextOptions<tst17_SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<tst17_SalesWebMvc.Models.Department> Department { get; set; }
+    }
+}
--- tst17-SalesWebMvc/Startup.cs
+++ tst17-SalesWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using tst17_SalesWebMvc.Data;
 
 namespace tst17_SalesWebMvc
 {
@@ -33,6 +35,9 @@ namespace tst17_SalesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<tst17_SalesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("tst17_SalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
