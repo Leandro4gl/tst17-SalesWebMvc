230f1c3d5e6730b22adcc783e3d888b111a53ccc
Leandro Oliveira <leandro4gl@gmail.com>
2020-04-27 17:25:59 -0300

    SellerService and basic FindAll

---
+++ tst17-SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tst17_SalesWebMvc.Models;
+using tst17_SalesWebMvc.Data;
+
+namespace tst17_SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly tst17_SalesWebMvcContext _context;
+
+        public SellerService(tst17_SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- tst17-SalesWebMvc/Startup.cs
+++ tst17-SalesWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using tst17_SalesWebMvc.Data;
+using tst17_SalesWebMvc.Services;
 
 namespace tst17_SalesWebMvc
 {
@@ -41,6 +42,7 @@ namespace tst17_SalesWebMvc
                         builder => builder.MigrationsAssembly("tst17_SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
