c1f8ba32a38e7ada920db04e142a19d511ad1610
Leandro de Oliveira <leandro4gl@gmail.com>
2020-04-29 18:08:08 -0300

    Validation

--- tst17-SalesWebMvc/Models/Seller.cs
+++ tst17-SalesWebMvc/Models/Seller.cs
@@ -8,16 +8,27 @@ namespace tst17_SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Campo {0} Obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} Tamanho Pequeno")]
         public string Name { get; set; }
+        
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Campo {0} Obrigatório")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         public string Email { get; set; }
+        
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "Campo {0} Obrigatório")]
         public DateTime BirthDate { get; set; }
+        
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary{ get; set; }
+        
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
 
